8878520cd1c222b9655626a006153a0edf4a8c9c
Kit <kit.j.lau@outlook.com>
2023-02-05 18:09:21 +0800

    fix: fixed the issue of white screen caused by switching pages under the dark theme

--- staticfiles/script.js
+++ staticfiles/script.js
@@ -1,7 +1,9 @@
-'use strict'
+'use strict';
+const moonIcon = document.getElementById('moon');
+const sunIcon = document.getElementById('sun');
+const themeButton = document.getElementById('change-mode');
+
 function changeModeIcon(isLight) {
-    const moonIcon = document.getElementById('moon');
-    const sunIcon = document.getElementById('sun');
     if (isLight) {
         moonIcon.removeAttribute('hidden');
         sunIcon.setAttribute('hidden', true);
@@ -12,14 +14,7 @@ function changeModeIcon(isLight) {
 }
 
 function switchTheme(isLight) {
-    const lightHL1 = document.getElementById('light-css-1');
-    const lightHL2 = document.getElementById('light-css-2');
-    const darkHL1 = document.getElementById('dark-css-1');
-    const darkHL2 = document.getElementById('dark-css-2');
-
-    const LS = window.localStorage;
     const LSisLight = LS.getItem('KitBlogIsLight') === 'true';
-
     isLight = isLight ?? !LSisLight;
 
     changeModeIcon(isLight);
@@ -32,19 +27,17 @@ function switchTheme(isLight) {
     LS.setItem('KitBlogIsLight', isLight);
 }
 
-const themeButton = document.getElementById('change-mode');
-const LS = window.localStorage;
-const LSisLight = LS.getItem('KitBlogIsLight') === 'true';
-
 themeButton.addEventListener('click', () => {
     switchTheme();
 });
 
-LS.getItem('KitBlogIsLight') === null
-    ? switchTheme(
+if (LS.getItem('KitBlogIsLight') === null) {
+    switchTheme(
         !(
             window.matchMedia &&
             window.matchMedia('(prefers-color-scheme: dark)').matches
         )
-    )
-    : switchTheme(LSisLight);
+    );
+} else {
+    switchTheme(LSisLight);
+}
