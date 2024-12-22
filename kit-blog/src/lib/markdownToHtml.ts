19b4dd3580519447a867d98b072df2353eb48cb1
Kit086 <kit.j.lau@outlook.com>
2024-12-22 22:43:02 +0800

    fix: 解决 markdownToHtml.ts remark use remarkSlug 报错的问题

--- kit-blog/src/lib/markdownToHtml.ts
+++ kit-blog/src/lib/markdownToHtml.ts
@@ -4,18 +4,22 @@ import { visit } from 'unist-util-visit';
 import remarkToc from 'remark-toc';
 import remarkGfm from 'remark-gfm';
 import remarkSlug from 'remark-slug';
+import { Plugin } from 'unified'
+import { Root } from 'mdast'
 
-function createImagePathPlugin(slug: string) {
-  return () => (tree: any) => {
-    visit(tree, 'image', (node: any) => {
-      // 如果是绝对路径或外部链接，不做处理
-      if (node.url.startsWith('http') || node.url.startsWith('/')) {
-        return;
-      }
-      
-      // 将相对路径转换为 /assets/[slug]/ 开头的路径
-      node.url = `/assets/${slug}/${node.url}`;
-    });
+function createImagePathPlugin(slug: string): Plugin<[], Root> {
+  return () => {
+    return (tree) => {
+      visit(tree, 'image', (node: any) => {
+        // 如果是绝对路径或外部链接，不做处理
+        if (node.url.startsWith('http') || node.url.startsWith('/')) {
+          return;
+        }
+        
+        // 将相对路径转换为 /assets/[slug]/ 开头的路径
+        node.url = `/assets/${slug}/${node.url}`;
+      });
+    };
   };
 }
 
@@ -26,7 +30,7 @@ export async function markdownToHtml(markdown: string, slug: string) {
     : '## Table of Contents\n\n' + markdown;
 
   const result = await remark()
-    .use(remarkSlug)  // 添加标题的 id
+    .use(remarkSlug as any)  // Type assertion to bypass the version mismatch
     .use(remarkToc, {
       heading: 'Table of Contents',
       tight: true,
